bc75feedec978e97db39953e1e1a9dc8b641e372
pikselan <faif@pikselan.com>
2020-07-16 23:51:17 +0700

    post image but cant over size

--- src/pages/private/parts/Post.tsx
+++ src/pages/private/parts/Post.tsx
@@ -4,9 +4,11 @@ import TimeAgo from "react-timeago";
 import axios from "../../../utils/axios";
 import { toast } from "react-toastify";
 import { Button } from "../../../components/Link";
+import imageCompression from "browser-image-compression";
 
 export default function Post(props: any) {
   const [posts, setPosts] = useState([]);
+  const [image, setImage] = useState({ preview: "", raw: "" });
 
   const getAllPosts = async () => {
     await axios
@@ -39,12 +41,19 @@ export default function Post(props: any) {
   const postStatus = async (e: any) => {
     e.preventDefault();
 
+    // const formData = new FormData();
+    // formData.set("text", e.target.status.value);
+    // formData.append("image", image.raw, `${Date.now()}_${Math.random().toString(36).substr(2)}.`);
+
     const statusData = {
       text: e.target.status.value,
+      image: image.raw,
     };
 
     e.target.status.value = "";
     e.target.status.rows = 1;
+    setImage({ preview: "", raw: "" });
+
     await axios
       .post("/users/post", statusData)
       .then((res) => {
@@ -54,6 +63,45 @@ export default function Post(props: any) {
         toast.dark("Network unavailable, try again");
       });
     getAllPosts();
+  };
+
+  const handleImageCompressed = (preview: any, raw: any) => {
+    setImage({
+      preview,
+      raw,
+    });
+  };
+
+  const handleImage = async (e: any) => {
+    if (e.target.files[0]) {
+      const imageFile = e.target.files[0];
+
+      const options = {
+        maxSizeMB: 1,
+        maxWidthOrHeight: 1024,
+        useWebWorker: true,
+      };
+      try {
+        const compressedFile = await imageCompression(imageFile, options);
+
+        let reader = new FileReader();
+        reader.readAsDataURL(compressedFile);
+        // reader.readAsBinaryString(compressedFile);
+        reader.onload = (i: any) => {
+          handleImageCompressed(
+            URL.createObjectURL(compressedFile),
+            i.target.result
+          );
+        };
+
+        // handleImageCompressed(
+        //   URL.createObjectURL(compressedFile),
+        //   compressedFile
+        // );
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   useEffect(() => {
@@ -73,18 +121,35 @@ export default function Post(props: any) {
               onFocus={(e) => (e.target.rows = 3)}
               placeholder="Post a status"
             ></textarea>
-            {/* <div className="form-file form-file-sm mt-2">
-              <Button className="btn-secondary btn-sm">Add Image</Button>
-              <input
-                type="file"
-                className="form-file-input"
-                id="customFileSm"
-              />
-              <label className="form-file-label" htmlFor="customFileSm">
-                <span className="form-file-text">Add image</span>
-                <span className="form-file-button">Browse</span>
+
+            <div className="image-upload mt-2">
+              {image.preview !== "" ? (
+                <img className="rounded w-100" src={image.preview} alt="" />
+              ) : (
+                ""
+              )}
+              <label htmlFor="file-input" style={{ cursor: "pointer" }}>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5 3C4.46957 3 3.96086 3.21071 3.58579 3.58579C3.21071 3.96086 3 4.46957 3 5V19C3 19.5304 3.21071 20.0391 3.58579 20.4142C3.96086 20.7893 4.46957 21 5 21H14.09C14.03 20.67 14 20.34 14 20C14 19.32 14.12 18.64 14.35 18H5L8.5 13.5L11 16.5L14.5 12L16.73 14.97C17.7 14.34 18.84 14 20 14C20.34 14 20.67 14.03 21 14.09V5C21 4.46957 20.7893 3.96086 20.4142 3.58579C20.0391 3.21071 19.5304 3 19 3H5ZM19 16V19H16V21H19V24H21V21H24V19H21V16H19Z"
+                    fill="grey"
+                  />
+                </svg>
               </label>
-            </div> */}
+              <input
+                id="file-input"
+                className="d-none"
+                type="file"
+                accept="image/*"
+                onChange={handleImage}
+              />
+            </div>
             <Button type="submit" className="btn-primary btn-sm mt-3 mr-3">
               Post a status
             </Button>
@@ -107,7 +172,11 @@ export default function Post(props: any) {
                 </span>
               </div>
             </div>
-            <img src={item.image} className="card-img-top rounded-0" alt="" />
+            <img
+              src={`${process.env.REACT_APP_HOST}/${item.image}`}
+              className="card-img-top rounded-0"
+              alt=""
+            />
             <div className="card-body">
               <span className="card-text">
                 <div className="post">
